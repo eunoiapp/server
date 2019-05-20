20ad6dd4190fd09b21cd9335254487cc5fadae74
Kristin <krik@itu.dk>
2019-05-20 19:37:02 +0200

    bug fix

--- about/index.py
+++ about/index.py
@@ -1,11 +1,15 @@
-from flask import Flask, Response, __version__
+from flask import Flask, Response, request, __version__
 app = Flask(__name__)
-source = 'https://github.com/zeit/now-examples/tree/master/python-flask'
-css = '<link rel="stylesheet" href="/css/style.css" />'
 
-@app.route('/', defaults={'path': ''})
-@app.route('/<path:path>')
-def catch_all(path):
-    name = path.upper()
-    v = __version__
-    return Response("%s<h1>%s</h1><ul><li>WSGI Enabled</li><li>Flask version <em>%s</em></li></ul><p>Visit the <a href='./'>home</a> page or view the <a href='%s'>source code</a>.</p>" % (css, name, v, source), mimetype='text/html')
+locations = {'Jens': 'home',
+            'Kristin' : 'work'}
+
+
+@app.route('/location/<name>', methods=['GET', 'PUT'])
+def location(name):
+    if request.method == 'PUT':
+        locations[name] = str(request.get_data())
+        print("location: " + locations[name])
+        return ''
+    else:
+        return locations[name]
