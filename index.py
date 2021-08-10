ecfd6429812e3175bb71c2f707c4ae566af724be
Kristin <krik@itu.dk>
2021-08-10 20:15:27 +0200

    changed db

--- index.py
+++ index.py
@@ -1,15 +1,19 @@
-from flask import Flask, Response, request, __version__
+from flask import Flask, Response, request, __version__, jsonify
+import json
 app = Flask(__name__)
 
-locations = {'Jens': 'home',
-            'Kristin' : 'home'}
+#locations = {'Jens': 'home','Kristin' : 'home'}
 
 
 @app.route('/location/<name>', methods=['GET', 'PUT'])
 def location(name):
+    with open('./data/locations.json', 'r') as f:
+        data = f.read()
+        locations = json.loads(data)
     if request.method == 'PUT':
         locations[name] = str(request.get_data())
-        print("location: " + locations[name])
-        return ''
+        with open('./data/locations.json', 'w') as f:
+            f.write(json.dumps(locations,))
+        return jsonify(locations[name])
     else:
         return locations[name]
